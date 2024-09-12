4975308337993644fa83bcdd65e0da300d0d6b68
JRParreno <jhonrhayparreno22@gmail.com>
2024-09-12 09:33:01 +0800

    added profile view

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -4,6 +4,10 @@ import 'package:etugal_flutter/core/common/widgets/custom_elevated_btn.dart';
 import 'package:etugal_flutter/core/config/shared_prefences_keys.dart';
 import 'package:etugal_flutter/core/notifier/shared_preferences_notifier.dart';
 import 'package:etugal_flutter/features/profile/presentation/widgets/index.dart';
+import 'package:etugal_flutter/features/task/domain/entities/index.dart';
+import 'package:etugal_flutter/features/task/presentation/blocs/tasks/task_provider_review/task_provider_review_bloc.dart';
+import 'package:etugal_flutter/features/task/presentation/pages/body/task_detail/index.dart';
+import 'package:etugal_flutter/features/task/presentation/pages/body/task_provider/index.dart';
 import 'package:etugal_flutter/gen/colors.gen.dart';
 import 'package:etugal_flutter/router/app_routes.dart';
 import 'package:flutter/material.dart';
@@ -18,11 +22,51 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
-class _ProfilePageState extends State<ProfilePage> {
+class _ProfilePageState extends State<ProfilePage>
+    with TickerProviderStateMixin {
+  late TaskUserProfileEntity userProfile;
+  late final TabController _mainTabCtrl;
+  final scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    final userCubitState = context.read<AppUserCubit>().state;
+    if (userCubitState is AppUserLoggedIn) {
+      userProfile = TaskUserProfileEntity(
+        id: int.parse(userCubitState.user.profilePk),
+        user: TaskUserEntity(
+            pk: int.parse(userCubitState.user.pk),
+            email: userCubitState.user.email,
+            firstName: userCubitState.user.firstName,
+            lastName: userCubitState.user.lastName,
+            username: userCubitState.user.username,
+            getFullName:
+                '${userCubitState.user.firstName} ${userCubitState.user.lastName}'),
+        birthdate: DateTime.now(),
+        address: userCubitState.user.address,
+        contactNumber: userCubitState.user.contactNumber,
+        gender: userCubitState.user.gender,
+        verificationStatus: userCubitState.user.verificationStatus,
+        profilePhoto: userCubitState.user.profilePhoto,
+        verificationRemarks: userCubitState.user.verificationRemarks,
+      );
+
+      context.read<TaskProviderReviewBloc>().add(
+            GetTaskProviderReviewEvent(
+                int.parse(userCubitState.user.profilePk)),
+          );
+    }
+
+    _mainTabCtrl = TabController(length: 2, vsync: this);
+
+    handleEventScrollListener();
+  }
+
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
-
+    final textTheme = Theme.of(context).textTheme;
     return Scaffold(
       body: SafeArea(
         child: BlocBuilder<AppUserCubit, AppUserState>(
@@ -46,19 +90,65 @@ class _ProfilePageState extends State<ProfilePage> {
                         topRight: Radius.circular(25),
                       ),
                     ),
-                    child: Column(
-                      children: [
-                        Align(
-                          alignment: Alignment.topRight,
-                          child: IconButton(
-                            onPressed: handleOnTapLogout,
-                            icon: const Icon(
-                              Icons.settings_outlined,
-                              size: 32,
-                            ),
+                    child: Container(
+                      margin:
+                          EdgeInsets.only(top: mediaQuery.size.height * 0.14),
+                      child: Column(
+                        children: [
+                          TabBar(
+                            controller: _mainTabCtrl,
+                            labelStyle: textTheme.bodyMedium
+                                ?.copyWith(color: ColorName.primary),
+                            indicatorColor: ColorName.primary,
+                            dividerHeight: 0,
+                            tabs: const [
+                              Tab(
+                                child: Text(
+                                  'Reviews',
+                                ),
+                              ),
+                              Tab(
+                                child: Text(
+                                  'About',
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
+                          Expanded(
+                            child: TabBarView(
+                              controller: _mainTabCtrl,
+                              children: <Widget>[
+                                ProviderReviewList(
+                                  controller: scrollController,
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(25),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(25),
+                                    border: Border.all(
+                                        color: ColorName.borderColor),
+                                  ),
+                                  child: PerformerInfo(
+                                    taskUserProfile: userProfile,
+                                    isHideHeader: true,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: mediaQuery.size.height * 0.19,
+                    right: 10,
+                    child: IconButton(
+                      onPressed: handleOnTapLogout,
+                      icon: const Icon(
+                        Icons.settings_outlined,
+                        size: 32,
+                      ),
                     ),
                   ),
                   Align(
@@ -108,5 +198,16 @@ class _ProfilePageState extends State<ProfilePage> {
         context.go(AppRoutes.onBoarding.path);
       }
     }
+  }
+
+  void handleEventScrollListener() {
+    scrollController.addListener(() {
+      if (scrollController.position.pixels >
+          (scrollController.position.pixels * 0.75)) {
+        context.read<TaskProviderReviewBloc>().add(
+              PaginateTaskProviderReviewEvent(userProfile.id),
+            );
+      }
+    });
   }
 }
